34c4894b9267fba267c9c6710622dfecc14f8f8c
Nakao2k <abcdown55@outlook.jp>
2022-09-07 08:17:04 +0900

    Added Netgear_XS512EM_vlan_pvidsetting.dat

--- HtmlToText.bat
+++ HtmlToText.bat
@@ -1,4 +1,6 @@
 HtmlToText.py "Netgear_XS512EM_sysInfo.dat" "Netgear" "^[^_]+_[^_]+_sysInfo\.html$"
-HtmlToText.py "Nec_AtermWX3000HP_infoMapeMain.dat" "Nec" "^[^_]+_[^_]+_info_mape_main\.html$"
+HtmlToText.py "Netgear_XS512EM_vlan_pvidsetting.dat" "Netgear" "^[^_]+_[^_]+_vlan_pvidsetting\.html$"
+
+HtmlToText.py "Nec_AtermWX3000HP_info_mape_main.dat" "Nec" "^[^_]+_[^_]+_info_mape_main\.html$"
 
 pause
