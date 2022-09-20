6d784894dd814f8dceedcac2258104e2eff31113
Nakao2k <abcdown55@outlook.jp>
2022-09-20 21:19:25 +0900

    パスワードを聞くプロンプトを追加

--- ScrapeNetworkDevice.bat
+++ ScrapeNetworkDevice.bat
@@ -1,3 +1,9 @@
-ScrapeNetworkDevice.py 192.168.0.239 "" Password123 Netgear_XS512EM.json Netgear 1
-ScrapeNetworkDevice.py 192.168.0.1 admin password Nec_AtermWX3000HP.json NEC 3
+
+@echo off
+set /p strPassword="対象ネットワーク機器のパスワードを入力してください(Default=%strPassword%)："
+
+@echo on
+ScrapeNetworkDevice.py 192.168.0.239 "" %strPassword% Netgear_XS512EM.json Netgear 1
+:: ScrapeNetworkDevice.py 192.168.0.1 admin %strPassword% Nec_AtermWX3000HP.json NEC 3
+
 pause
